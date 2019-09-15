9ea10b2fbba562c7ca23c604fc75e717462d9b9c
sanjiv sahayam <sanjsmailbox@gmail.com>
2019-09-15 22:26:48 +1000

    More refactoring

--- src/Milo/Oauth2.hs
+++ src/Milo/Oauth2.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Milo.Oauth2 where
 
 import qualified Network.OAuth               as OA
@@ -6,7 +8,8 @@ import qualified Data.ByteString.Char8       as C8
 import qualified Data.ByteString.Lazy        as LS
 import qualified Network.HTTP.Client         as Client
 import qualified Network.HTTP.Client.TLS     as Client
-import Network.HTTP.Types.Header (hAuthorization)
+import qualified Network.HTTP.Types          as Client
+import qualified Network.HTTP.Types.Header   as Client
 import Data.CaseInsensitive (mk)
 import Milo.Config
 import Milo.Model
@@ -20,16 +23,37 @@ makeRequest manager req = do
     resp <- Client.httpLbs req manager
     return $ eitherDecodeStrict' (LS.toStrict $ Client.responseBody resp)
 
-performAction :: (FromJSON a, FromJSON b) => Env -> Client.Manager -> (OAuth2ClientToken -> RequestProvider IO a) -> (a -> RequestProvider IO b) -> IO (Either String b)
-performAction env manager reqProviderA reqProviderB = do
+performAction :: FromJSON b => Env -> Client.Manager -> (BearerToken -> RequestProvider IO b) -> IO (Either String b)
+performAction env manager reqProviderB = do
   let 
-      clientKey         = _clientKey $ env
-      clientSecret      = _clientSecret $ env
+      clientKey         = _clientKey env
+      clientSecret      = _clientSecret env
       clientToken       = OAuth2ClientToken clientKey clientSecret
-  tokenResponseE <- (getRequest $ reqProviderA clientToken) >>= makeRequest manager
+  tokenResponseE <- getRequest (bearerRequestProvider clientToken) >>= makeRequest manager
   case tokenResponseE of
-    Right bearer -> (getRequest $ reqProviderB bearer) >>= makeRequest manager
+    Right bearer -> getRequest (reqProviderB bearer) >>= makeRequest manager
     Left error -> pure . Left $ error
 
 tap :: Show a => IO a -> (a -> IO ()) -> IO a
 tap ioa f = ioa >>= f >> ioa
+
+--- access token retrieval 
+tokenUrl :: String
+tokenUrl = "https://api.twitter.com/oauth2/token"
+
+bearerRequestProvider :: OAuth2ClientToken -> RequestProvider IO BearerToken
+bearerRequestProvider oauth2ClientToken = 
+  RequestProvider $ addBasicAuth oauth2ClientToken . addTokenFormParams <$> Client.parseRequest tokenUrl
+
+addBasicAuth :: OAuth2ClientToken -> Client.Request -> Client.Request
+addBasicAuth (OAuth2ClientToken (ClientKey key) (ClientSecret secret)) = Client.applyBasicAuth key secret
+
+addBearerTokenAuth :: BearerToken -> Client.Request -> Client.Request
+addBearerTokenAuth (BearerToken _ accessToken) req = 
+  let authHeader = (Client.hAuthorization, "Bearer " <> C8.pack accessToken)
+      existingHeaders = Client.requestHeaders req
+      allHeaders = authHeader : existingHeaders
+  in req { Client.requestHeaders = allHeaders }
+
+addTokenFormParams :: Client.Request -> Client.Request
+addTokenFormParams = Client.urlEncodedBody [("grant_type", "client_credentials")]
--- src/Milo/UserTimeline/Service.hs
+++ src/Milo/UserTimeline/Service.hs
@@ -7,23 +7,14 @@ import Milo.Config
 import Milo.Model
 import Milo.Request
 import Milo.Oauth2.Model
--- import Milo.UserTimeline.Model
+
 import qualified Data.ByteString.Char8       as C8
 import qualified Network.HTTP.Client         as Client
 import qualified Network.HTTP.Client.TLS     as Client
 import qualified Network.HTTP.Types          as Client
-import Data.Aeson (fromJSON)
 
 getUserTimeline :: Env -> Client.Manager -> TwitterHandle -> IO (Either String [Tweet])
-getUserTimeline env manager tuser = performAction env manager bearerRequestProvider $ userRequestProvider tuser
-
---- access token retrieval 
-tokenUrl :: String
-tokenUrl = "https://api.twitter.com/oauth2/token"
-
-bearerRequestProvider :: OAuth2ClientToken -> RequestProvider IO BearerToken
-bearerRequestProvider oauth2ClientToken = 
-  RequestProvider $ (addBasicAuth oauth2ClientToken . addTokenFormParams) <$> Client.parseRequest tokenUrl
+getUserTimeline env manager = performAction env manager . userRequestProvider
 
 --- get mentions 
 userTimelineUrl :: String
@@ -31,23 +22,10 @@ userTimelineUrl = "https://api.twitter.com/1.1/statuses/user_timeline.json"
 
 userRequestProvider :: TwitterHandle -> BearerToken -> RequestProvider IO [Tweet]
 userRequestProvider tuser bearer =
-  RequestProvider $ (addBearerTokenAuth bearer . addQueryParams tuser) <$> Client.parseRequest userTimelineUrl
+  RequestProvider $ addBearerTokenAuth bearer . addQueryParams tuser <$> Client.parseRequest userTimelineUrl
 
 twitterHandleParam :: TwitterHandle -> (C8.ByteString, Maybe C8.ByteString)
 twitterHandleParam (TwitterHandle tuser)= ("screen_name", Just tuser)
-
-addBasicAuth :: OAuth2ClientToken -> Client.Request -> Client.Request
-addBasicAuth (OAuth2ClientToken (ClientKey key) (ClientSecret secret)) = Client.applyBasicAuth key secret
-
-addBearerTokenAuth :: BearerToken -> Client.Request -> Client.Request
-addBearerTokenAuth (BearerToken _ accessToken) req = 
-  let authHeader = (Client.hAuthorization, "Bearer " <> C8.pack accessToken)
-      existingHeaders = Client.requestHeaders req
-      allHeaders = authHeader : existingHeaders
-  in req { Client.requestHeaders = allHeaders }
-
-addTokenFormParams :: Client.Request -> Client.Request
-addTokenFormParams = Client.urlEncodedBody [("grant_type", "client_credentials")]
 
 addQueryParams :: TwitterHandle -> Client.Request -> Client.Request
 addQueryParams tuser = Client.setQueryString [countParam, twitterHandleParam tuser, extendedTweetParam]
