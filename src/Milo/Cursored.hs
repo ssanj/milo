b1190b1fe793f66816557fa2f03b4cee4ee0c63d
sanjiv sahayam <sanjsmailbox@gmail.com>
2020-01-02 00:51:30 +1100

    Fix haddock

--- src/Milo/Cursored.hs
+++ src/Milo/Cursored.hs
@@ -1,32 +1,32 @@
 {-# LANGUAGE ScopedTypeVariables   #-}
 
 module Milo.Cursored (
+    -- * functions
     unfoldWith
-    -- ^ functions
+  
+    -- * data types
   , Cursor(..)
   , CursorState(..)
-  -- ^ data types
+  
 ) where
 
 -- | Wrapper around a cursor of some sort
 newtype Cursor a = Cursor a deriving Eq
 
 -- | An ADT to capture the three states of a cursor:
--- NewCursor - Initial cursor state
--- GoCursor - A state of having a cursor, typically used for iteration
--- StopCursor - The final cursor state
-data CursorState a = NewCursor | GoCursor (Cursor a) | StopCursor
+data CursorState a 
+  = NewCursor           -- ^ Initial request - there is no cursor; the iteration will start at this state
+  | GoCursor (Cursor a) -- ^ A state of having a cursor; the iteration will continue in this state
+  | StopCursor          -- ^ The final cursor state; the iteration will stop at this state
 
 -- | A Generic function that unfolds a computation that has some form of 'Cursor'; such as a paginated REST API.
-unfoldWith :: forall m a b c. Applicative m => 
-  (a -> (b, CursorState c)) -> 
-  -- ^ a function that can extract a "result" and the next 'CursorState' from some response 'a'
-  (Maybe (Cursor c) -> m a)   -> 
-  -- ^ a function that takes an optional 'Cursor' and returns a response 'a' in the effect 'm'
-  (CursorState c)            -> 
-  -- ^ an initial 'CursorState'
-  m (Maybe (b, CursorState c))
-  -- ^ the optional pair of the result type 'b' with the next 'CursorState' in the effect 'm'
+unfoldWith :: forall m a b c. Applicative m 
+           => 
+           (a -> (b, CursorState c))        -- ^ a function that can extract a "result" and the next 'CursorState' from some response 'a'
+           -> (Maybe (Cursor c) -> m a)     -- ^ a function that takes an optional 'Cursor' and returns a response 'a' in the effect 'm'
+           -> (CursorState c)               -- ^ an initial 'CursorState'
+           ->  m (Maybe (b, CursorState c)) -- ^ the optional pair of the result type 'b' with the next 'CursorState' in the effect 'm'
+  
 unfoldWith extractPayload iterateWith NewCursor = 
   let resultM :: m a = iterateWith Nothing
   in  Just . extractPayload <$> resultM
